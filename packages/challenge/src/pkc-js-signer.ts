ea03c82a3b50178d1fc22340eb50c4cfbaeee046
Tommaso Casaburi <tom@bitsocial.net>
2026-05-08 16:50:59 +0700

    fix(challenge): trim runtime dependencies

--- packages/challenge/src/pkc-js-signer.ts
+++ packages/challenge/src/pkc-js-signer.ts
@@ -1,20 +1,14 @@
-import path from "path";
-import { createRequire } from "module";
-import { pathToFileURL } from "url";
 import * as ed from "@noble/ed25519";
 import { fromString as uint8ArrayFromString } from "uint8arrays/from-string";
-
-const require = createRequire(import.meta.url);
-const pkcEntry = require.resolve("@pkcprotocol/pkc-js");
-const pkcRoot = path.resolve(path.dirname(pkcEntry), "..", "..");
-
-const signerUtilUrl = pathToFileURL(path.join(pkcRoot, "dist/node/signer/util.js")).href;
-
-const [{ getPublicKeyFromPrivateKey }] = await Promise.all([import(signerUtilUrl)]);
+import { toString as uint8ArrayToString } from "uint8arrays/to-string";
 
 export const signBufferEd25519 = async (bufferToSign: Uint8Array, privateKeyBase64: string) => {
     const privateKeyBuffer = uint8ArrayFromString(privateKeyBase64, "base64");
     return ed.sign(bufferToSign, privateKeyBuffer);
 };
 
-export { getPublicKeyFromPrivateKey };
+export const getPublicKeyFromPrivateKey = async (privateKeyBase64: string) => {
+    const privateKeyBuffer = uint8ArrayFromString(privateKeyBase64, "base64");
+    const publicKeyBuffer = await ed.getPublicKey(privateKeyBuffer);
+    return uint8ArrayToString(publicKeyBuffer, "base64");
+};
